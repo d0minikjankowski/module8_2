8eb7f236012a2a02dff9257e0555198f021b02a1
Dominik Jankowski <dominik.jankowski@outlook.com>
2025-05-21 21:41:28 +0200

    feat:8-5-testy-procesu-zakupoweg-init

---
+++ cypress/e2e/TS2.cy.js
@@ -0,0 +1,16 @@
+import HomePage from "../page_objects/homePage";
+import AccountPage from "../page_objects/homePage";
+import CartPage from "../page_objects/cartPage";
+
+
+describe('my first scenario', () => {
+    const homePage = new HomePage();
+    const accountPage = new AccountPage();
+    const cartPage = new CartPage();
+
+
+
+
+
+
+})
